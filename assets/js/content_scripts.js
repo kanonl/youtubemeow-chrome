dda7b0e0cde0115241a9699e46b7d88e43343c38
Kanon Lo <klo@jsls.com>
2018-03-28 11:18:54 -0400

    Initial commit

---
+++ assets/js/content_scripts.js
@@ -0,0 +1,25 @@
+(function () {
+    let el = document.querySelector("#hdtb-msb-vis");
+
+    if (el) {
+        let url = new URL(window.location.href);
+
+        let div = document.createElement("div");
+        div.classList.add("hdtb-mitem");
+        div.classList.add("hdtb-imb");
+
+        let yt = new URL("https://www.youtube.com");
+        yt.pathname = "/results";
+        yt.search = new URLSearchParams({ "search_query": url.searchParams.get("q") });
+
+        let anchor = document.createElement("a");
+        anchor.classList.add("q");
+        anchor.classList.add("qs");
+        anchor.href = yt.toString();
+        // anchor.setAttribute("target", "_blank");
+
+        anchor.appendChild(document.createTextNode("YouTube"));
+        div.appendChild(anchor);
+        el.appendChild(div);
+    }
+})();
