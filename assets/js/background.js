5f7c9b2f43abc0ddfad5e113b0355266a1251ec5
Kanon Lo <15108745+kanonl@users.noreply.github.com>
2018-11-02 09:02:00 -0400

    Refactor/clean up

--- assets/js/background.js
+++ assets/js/background.js
@@ -1,26 +1,24 @@
 (function () {
     "use strict";
-    
-    const menuItemId = "3391dbce625f4f0980ddbe2c95dfe514";
 
-    const contextMenus = () => {
-        let createProperties = {
-            id: menuItemId,
-            title: "Search YouTube for \"%s\"",
-            contexts: ["selection"]
-        };
-
-        chrome.contextMenus.create(createProperties, () => {
-            if (chrome.runtime.lastError) console.error(chrome.runtime.lastError.message);
-        });
+    const createHandler = () => {
+        if (chrome.runtime.lastError) console.error(chrome.runtime.lastError.message);
     };
 
-    chrome.runtime.onInstalled.addListener(event => contextMenus());
+    const contextMenus = () => {
+        chrome.contextMenus.create({
+            id: 'youtube',
+            title: "Search YouTube for \"%s\"",
+            contexts: ["selection"]
+        }, createHandler);
+    };
 
-    chrome.runtime.onStartup.addListener(event => contextMenus());
+    chrome.runtime.onInstalled.addListener(contextMenus);
+
+    chrome.runtime.onStartup.addListener(contextMenus);
 
     chrome.contextMenus.onClicked.addListener((info, tab) => {
-        if (info.menuItemId === menuItemId) {
+        if (info.menuItemId === 'youtube') {
             let url = new URL("https://www.youtube.com");
             url.pathname = "/results";
             url.search = new URLSearchParams({ "search_query": info.selectionText });
